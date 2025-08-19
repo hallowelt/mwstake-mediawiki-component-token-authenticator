ccb36000a383890850b647106024ea6318595f2c
d.savuljesku <d.savuljesku@gmail.com>
2025-08-19 15:58:43 +0200

    Update docs

--- bootstrap.php
+++ bootstrap.php
@@ -4,7 +4,7 @@ if ( defined( 'MWSTAKE_MEDIAWIKI_COMPONENT_TOKEN_AUTHENTICATOR_VERSION' ) ) {
 	return;
 }
 
-define( 'MWSTAKE_MEDIAWIKI_COMPONENT_TOKEN_AUTHENTICATOR_VERSION', '1.0.1' );
+define( 'MWSTAKE_MEDIAWIKI_COMPONENT_TOKEN_AUTHENTICATOR_VERSION', '1.0.2' );
 
 MWStake\MediaWiki\ComponentLoader\Bootstrapper::getInstance()
 ->register( 'token-authenticator', static function () {
--- resources/bootstrap.js
+++ resources/bootstrap.js
@@ -2,20 +2,22 @@ mws = window.mws || {};
 mws.tokenAuthenticator = {
 	generateToken: ( withIssuer ) =>  {
 		const dfd = $.Deferred();
-		$.ajax( {
-			url: mw.util.wikiScript( 'rest' ) + '/mws/v1/user-token/generate?withIssuer=' + ( withIssuer ? 'true' : 'false' ),
-			dataType: 'json',
-			type: 'GET',
-			contentType: 'application/json; charset=utf-8'
-		} ).done(  ( data ) => {
-			if ( data && data.value ) {
-				dfd.resolve( data.value );
-			} else {
+		mw.loader.using( 'mediawiki.util' ).then( () => {
+			$.ajax( {
+				url: mw.util.wikiScript( 'rest' ) + '/mws/v1/user-token/generate?withIssuer=' + ( withIssuer ? 'true' : 'false' ),
+				dataType: 'json',
+				type: 'GET',
+				contentType: 'application/json; charset=utf-8'
+			} ).done(  ( data ) => {
+				if ( data && data.value ) {
+					dfd.resolve( data.value );
+				} else {
+					dfd.reject();
+				}
+			} ).fail( () => {
 				dfd.reject();
-			}
-		} ).fail( () => {
-			dfd.reject();
-		} );
+			} );
+		}, () => { dfd.reject(); } );
 		return dfd.promise();
 	}
 };
